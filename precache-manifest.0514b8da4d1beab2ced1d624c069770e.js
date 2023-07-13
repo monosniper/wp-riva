588cde0354071de241e523686b11ad1742cb51ef
monosniper <vanteateam@gmail.com>
2023-07-14 00:47:08 +0500

    asdasd

---
+++ precache-manifest.0514b8da4d1beab2ced1d624c069770e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfbcbfd4c9b6b73e3fd6b2c46983945",
+    "url": "/wp-content/themes/wp-riva/index.php"
+  },
+  {
+    "revision": "77cad26983ef16cfd586",
+    "url": "/wp-content/themes/wp-riva/static/css/2.chunk.css"
+  },
+  {
+    "revision": "6925ed1fbdbe9332f606",
+    "url": "/wp-content/themes/wp-riva/static/css/main.chunk.css"
+  },
+  {
+    "revision": "77cad26983ef16cfd586",
+    "url": "/wp-content/themes/wp-riva/static/js/2.chunk.js"
+  },
+  {
+    "revision": "5beed8e2b52ad8721bd37cdbe5d663ee",
+    "url": "/wp-content/themes/wp-riva/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed23d61a0113bba9a61",
+    "url": "/wp-content/themes/wp-riva/static/js/bundle.js"
+  },
+  {
+    "revision": "6925ed1fbdbe9332f606",
+    "url": "/wp-content/themes/wp-riva/static/js/main.chunk.js"
+  }
+]);
